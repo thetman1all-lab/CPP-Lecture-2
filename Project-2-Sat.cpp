ca138b13eb78e6eceaaf46b0fe8d847d05c2536c
tcounihan1 <thetman1all@gmail.com>
2026-02-07 15:22:41 -0800

    Removed non standard C++ variable and replaced it with an int. Used += and -- for more efficient variable manipulation. Countdown counts to 0 without using up entire counsil for large sums. Total sum now prints outside of for loop. <iostream> was still at the top of my script.

--- Project-2-Sat.cpp
+++ Project-2-Sat.cpp
@@ -4,45 +4,45 @@ int main() {
 
     //Declare repeating variable
     char repeat_input;
-    int repeat;
+    bool repeat;
 
     do {
     // Decalring variables
     int N;
     int total = 0;
     
+    
     // Prompt user to input N
-    std::cout << "Input how many times to loop:" << std::endl;
+    std::cout << "Input how many times to loop:";
     std::cin >> N;
-
-    // Declare empty array of size N that will be used to store input values
-    int number_inputs[N];
 
     // for loop to prompt user to input N amount of integers
     for (int i = 0; i < N; i++) {
-        std::cout << "Enter a number:" << std::endl;
-        std::cin >> number_inputs[i];
+        // Declare number inside for loop to refresh each itteration
+        int number;
+        std::cout << "Enter a number: ";
+        std::cin >> number;
 
         // Add inputs together and print output at end
-        total = total + number_inputs[i];
-        if (i==N-1) {
-            std::cout << "Total sum = " << total << std::endl;
-        }
+        total += number;
     }
 
+    std::cout << "Total sum: " << total << "\n" << std::endl;
+
     // While loop to count down the total sum
-    std::cout << "Counting Down:" << std::endl;
     while (total >= 0) {
-        std::cout << total << std::endl;
-        total = total - 1;
+        // Clears the line ABOVE the current cursor position using ANSI Escape Codes
+        std::cout << "\x1b[1A" << "\x1b[2K\r" << std::flush;
+        std::cout << "Counting Down: " << total << std::endl;
+        total --;
     }
 
     // Ask to repeat
-    std::cout << "Do you want to repeat? (y/n):" << std::endl;
+    std::cout << "Do you want to repeat? (y/n): ";
     std::cin >> repeat_input;
     repeat = (repeat_input == 'y');
 
-    } while (repeat == 1);
+    } while (repeat);
 
     return 0;
 }
