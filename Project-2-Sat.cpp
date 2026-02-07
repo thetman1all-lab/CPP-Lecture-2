a008ca518a45c07b519d6257a32daf18c530e96c
tcounihan1 <thetman1all@gmail.com>
2026-02-07 07:46:14 -0800

    Added comments of what to do for assignment, declared some variables, and made some prompts for the user. Not tested yet

--- Project-2-Sat.cpp
+++ Project-2-Sat.cpp
@@ -2,6 +2,30 @@
 #include <iomanip>
 #include <string>
 
+// - Uses a for loop to prompt N times for integers, sum them (store in int total = 0; add each input)
+
+// - After, use a while loop to print the sum repeatedly until it hits 0 (subtract 1 each time—silly but practices while)
+
+// - Bonus (optional for mastery): Wrap in do-while—ask if repeat (y/n), loop whole thing if 'y'
+
 int main() {
+
+    // Decalring variables
+    int N;
+
+    
+    // Prompt user to input N
+    std::cout << "Input how many times to loop:" << std::endl;
+    std::cin >> N;
+
+    // Declare empty array of size N that will be used to store input values
+    int number_inputs[N];
+
+    // for loop to prompt user to input N amount of integers
+    for (int i = 0; i < N; i++) {
+        std::cout << "Enter a number:" << std::endl;
+        std::cin >> number_inputs[i];
+    }
+
     return 0;
 }
