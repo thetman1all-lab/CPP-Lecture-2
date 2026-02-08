6d8c9c6a2a7fae2bd5d0d02658d67241e984bfe3
tcounihan1 <thetman1all@gmail.com>
2026-02-07 17:22:05 -0800

    Added general framework for the simple menue for playing game or quitting

--- Project-2-Sun.cpp
+++ Project-2-Sun.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-#include <iomanip>
-#include <string>
+
 
 int main() {
-    
+    int choice;
+    do {
+        std::cout << "1. Play Game\n2. Quit\nChoise: ";
+        std::cin >> choice;
+        if (choice == 1) {
+            // Play Game
+
+        } else if (choice != 2) {
+            // Invalid choice
+            std::cout << "Not a valid choice!\n";
+            continue; //Skip back to menu
+        }
+    } while(choice != 2);
+
     return 0;
 }
